a71b344b42b87eb4a222937894bb2a6ba3b23d06
Hong Qin <qinstat@gmail.com>
2018-05-31 13:51:55 -0400

    Empty old folder

--- old/qinexg06data/M8/old/auto.mle.sh
+++
@@ -1,8 +0,0 @@
-cp /home/hqin/R/scripts/lab/auto.bootstrap.by.col.00.r .
-cp /home/hqin/R/scripts/lab/gompertz.mle.00.r .
-cp /home/hqin/R/scripts/lab/r.sh  .
-
-sh r.sh auto.bootstrap.by.col.00.r > _out.auto.bootstrap.by.col.00.r.txt 
-sh r.sh gompertz.mle.00.r > _out.gompertz.mle.00.r.txt 
-
-
--- old/readme.txt
+++
@@ -1,4 +0,0 @@
-2016 June 6 Tue. Hong Qin started this repository for preparing network aging manuscript for BMC systems biology. Qin moved some directories from  “0.network.aging.prj” and “mactower-network-failure-simulation”. 
-
-
-
